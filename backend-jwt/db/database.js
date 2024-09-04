5eeff91890ba7f2293440ff559b76796ec38bbce
Leonardo <leoonardoogzz@gmail.com>
2024-09-04 11:54:33 -0600

    Mofificaciones al backend-jwt

--- backend-jwt/db/database.js
+++ backend-jwt/db/database.js
@@ -1,25 +1,15 @@
-export const database = 
-    {
-        user: [
-            {
-                id: 1,
-                username: 'admin',
-                password: 'admin'
-            },
-            {
-                id: 2,
-                username: 'user',
-                password: 'user'
-            },
-            {
-                id: 3,
-                username: 'guest',
-                password: 'guest'
-            },
-            {
-                id: 4,
-                username: 'guest2',
-                password: 'guest2'
-            }
-        ]
-    }
+const mysql = require("mysql2");
+
+const connection = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+});
+
+connection.connect((err) => {
+  if (err) throw err;
+  console.log("Connected to database");
+});
+
+module.exports = connection;
