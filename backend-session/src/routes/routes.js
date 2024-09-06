e3dffce8600e282e90befb52c86c7d9bdabba99a
Leonardo <leoonardoogzz@gmail.com>
2024-09-06 15:39:04 -0600

    Ultimo commit

--- backend-session/src/routes/routes.js
+++ backend-session/src/routes/routes.js
@@ -1,13 +1,16 @@
 import { Router } from "express";
-import { login, session, logout, register } from "../controllers/controllers.js";
-export const sessionRoutes = Router()
+import {
+  login,
+  session,
+  logout,
+  register,
+} from "../controllers/controllers.js";
+export const sessionRoutes = Router();
 
-sessionRoutes.post('/login', login );
+sessionRoutes.post("/login", login);
 
-sessionRoutes.post('/register', register );
+sessionRoutes.post("/register", register);
 
-// Ruta para obtener los datos de la sesión
-sessionRoutes.get('/session', session );
+sessionRoutes.get("/session", session);
 
-// Ruta para cerrar la sesión
-sessionRoutes.post('/logout', logout );
+sessionRoutes.post("/logout", logout);
--- frontend/public/src/script/main.js
+++ frontend/public/src/script/main.js
@@ -1,32 +1,28 @@
 (async () => {
-    const response = await fetch('http://localhost:4000/session', {
-        method: 'GET',
-        credentials: 'include' // Importante para enviar las cookies de sesión
-    })
+  const response = await fetch("http://localhost:4000/session", {
+    method: "GET",
+    credentials: "include",
+  });
 
-    console.log({ response })
+  console.log({ response });
 
-
-    if (response.ok) {
-        const data = await response.json();
-        document.getElementById('user-name').innerText = data.user.username;
-    } else {
-        // Redirigir al usuario a la página de inicio de sesión
-        window.location.href = 'index.html';
-    }
+  if (response.ok) {
+    const data = await response.json();
+    document.getElementById("user-name").innerText = data.user.username;
+  } else {
+    window.location.href = "index.html";
+  }
 })();
 
+document.getElementById("logout").addEventListener("click", async () => {
+  const response = await fetch("http://localhost:4000/logout", {
+    method: "POST",
+    credentials: "include",
+  });
 
-// Manejar el cierre de sesión
-document.getElementById('logout').addEventListener('click', async () => {
-    const response = await fetch('http://localhost:4000/logout', {
-        method: 'POST',
-        credentials: 'include'
-    })
-    
-    if (!response.ok) {
-        throw new Error('Error al cerrar sesión');
-    } else {
-        window.location.href = 'index.html';
-    }
+  if (!response.ok) {
+    throw new Error("Error al cerrar sesión");
+  } else {
+    window.location.href = "index.html";
+  }
 });
